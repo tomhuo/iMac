a5e0543296e6faacfdb8ba1161bfa29ea69541e2
huojigang <huojigang@huojigangtekiMac.local>
2014-05-10 20:58:52 +0800

    slowDown

--- HearMeNow/ViewController.m
+++ HearMeNow/ViewController.m
@@ -81,6 +81,10 @@
         if (!error)
         {
             soundPlayer.delegate = self;
+            
+            soundPlayer.enableRate = YES;
+            soundPlayer.rate = 0.5;
+            
             [soundPlayer play];
         } else {
             NSLog(@"Error initializing player: %@", error);
